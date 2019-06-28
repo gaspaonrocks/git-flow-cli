75beb3bfd1cc217ac6968c1c8918a24bd0a4fe9e
Sacha GRASSO <sacha.grasso-ext@atinternet.com>
2019-06-28 17:20:06 +0200

    choosing the branch to checkout to

--- src/commands/checkout.ts
+++ src/commands/checkout.ts
@@ -5,7 +5,7 @@ import { sanitizeBranches2 } from "../utils"
 const message = "this is checkout, where to ?";
 
 // ts-ignore-next-line
-const localBranches = sanitizeBranches2(execSync('git branch', { encoding: 'utf8' }));
+const localBranches = ['new-branch', ...sanitizeBranches2(execSync('git branch', { encoding: 'utf8' }))];
 
 export default {
     message,
